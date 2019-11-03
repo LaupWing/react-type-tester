97db8bb1f2537da33592cffca70659ff04914e72
LaupWing <loc_strict@hotmail.com>
2019-11-03 19:28:39 +0100

    Changed the Output to  a component

--- src/Output/Output.js
+++ src/Output/Output.js
@@ -1,53 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Output.css'
-const Output = (props)=>{
-    const {userInput, timeLeft} = props
+class Output extends Component{
+
     // Output is one step behind maybe use redux to fix this problem?
-    const calcWordPM = ()=>{
-        if(userInput.length>0){
-            const totalWords = userInput.split(' ').filter(input=>input!=='').length
-            const wordPerMinute = (totalWords/(60-timeLeft)) * 60
+    calcWordPM = ()=>{
+        if(this.props.userInput.length>0){
+            const totalWords = this.props.userInput.split(' ').filter(input=>input!=='').length
+            const wordPerMinute = (totalWords/(60-this.props.timeLeft)) * 60
             
             return Math.floor(wordPerMinute)
         }else{
             return '0'
         }
     }
-    const incorrect = ()=>{
-        if(userInput.length >0){
+    incorrect = ()=>{
+        if(this.props.userInput.length >0){
             return document.querySelectorAll('.LiveView span.word.incorrect').length
         }
         return '0'
     }
-    const correct = ()=>{
-        if(userInput.length >0){
+    correct = ()=>{
+        if(this.props.userInput.length >0){
             return document.querySelectorAll('.LiveView span.word.correct').length
         }
         return '0'
     }
-    return(
-        <div className="Output">
-            <div className="field">
-                <p className="info">Time left</p>
-                <p className="outcome">{timeLeft}</p>
+    render(){
+        return(
+            <div className="Output">
+                <div className="field">
+                    <p className="info">Time left</p>
+                    <p className="outcome">{this.props.timeLeft}</p>
+                </div>
+    
+                <div className="field">
+                    <p className="info">Correct</p>
+                    <p className="outcome">{this.correct()}</p>
+                </div>
+    
+                <div className="field">
+                    <p className="info">Incorrect</p>
+                    <p className="outcome">{this.incorrect()}</p>
+                </div>
+    
+                <div className="field">
+                    <p className="info">Words per minute</p>
+                    <p className="outcome">{this.calcWordPM()}</p>
+                </div>
             </div>
-
-            <div className="field">
-                <p className="info">Correct</p>
-                <p className="outcome">{correct()}</p>
-            </div>
-
-            <div className="field">
-                <p className="info">Incorrect</p>
-                <p className="outcome">{incorrect()}</p>
-            </div>
-
-            <div className="field">
-                <p className="info">Words per minute</p>
-                <p className="outcome">{calcWordPM()}</p>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Output
